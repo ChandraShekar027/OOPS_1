7995ba02b2ff2de6be9ee10e55f0a4ceff131193
ChandraShekar027 <chandrashekargrl027@gmail.com>
2024-07-30 15:29:21 +0530

    Add project files.

---
+++ PolyMorphism_1/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyMorphism_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          Animal dog = new Dog();
+            dog.Sound();
+           
+        }
+    }
+    class Animal
+    {
+       public virtual void Sound()
+        {
+            Console.WriteLine("Makes some sound");
+        }
+    }
+
+    class Cat : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("meow meow");
+        }
+    }
+
+    class Dog : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("Bow Bow");
+            Console.ReadLine();
+        }
+    }
+
+}
