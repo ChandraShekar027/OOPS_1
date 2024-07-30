ba2c150330942c4a54b191ae03436b4bab621cac
ChandraShekar027 <chandrashekargrl027@gmail.com>
2024-07-30 16:10:50 +0530

    Add project files.

---
+++ Method_Overloading1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Overloading1
+{
+    internal class Program
+    {
+        public static void Disp(string s)
+        {
+            Console.WriteLine("display "+s);
+            Console.ReadLine();
+        }
+        public static void Disp(int i)
+        {
+            Console.WriteLine("display " + i);
+            Console.ReadLine();
+        }
+        public static void Disp(bool b)
+        {
+            Console.WriteLine("display " + b);
+            Console.ReadLine();
+        }
+        public static void Main(string[] args)
+        {
+            Disp(10);
+            Disp(false);
+            Disp("chandu");
+        }
+    }
+}
